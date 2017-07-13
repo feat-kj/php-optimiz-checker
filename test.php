0cf75ef5ecc87269c8c4a5a80a7339be21ed63d2
namamugi <feat2kj@gmail.com>
2017-07-14 01:43:51 +0900

    test.php update

--- test.php
+++ test.php
@@ -1,19 +1,19 @@
 <?php
-// use Foo\Bar;
-// class abc {
-//   public $value1='';
-//
-//   /**
-//    * @param integer $a
-//    * @param integer $b
-//    * @param integer $c
-//    */
-//   public function Execute($a,$b,$c){
-//     if($b){
-//     } else if {
-//     }
-//     $a=10;
-//     return;
-//   }
-// }
+use Foo\Bar;
+class abc {
+  public $value1='';
+
+  /**
+   * @param integer $a
+   * @param integer $b
+   * @param integer $c
+   */
+  public function Execute($a,$b,$c){
+    if($b){
+    } else if {
+    }
+    $a=10;
+    return;
+  }
+}
 ?>
