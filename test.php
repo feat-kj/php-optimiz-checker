98aed7ba10e9c022edeb7e574f467e1a81c2a6a1
namamugi <feat2kj@gmail.com>
2017-08-30 02:10:46 +0900

    testtest

--- test.php
+++ test.php
@@ -8,7 +8,7 @@ class abc {
    * @param integer $b
    * @param integer $c
    */
-  public function Execute($accv,$bc,$ccc){
+  public function Execute($accv,$bqc,$ccc){
     if($ccc){
       // aaaa
       $bcc = 2;
